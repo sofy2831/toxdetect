0c969b0e6f8094031731bc479178afc3dd798954
sofy2831 <s.dumas974@orange.fr>
2025-02-21 18:13:35 +0100

    Update urgences.js

--- urgences.js
+++ urgences.js
@@ -22,9 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             selectedContacts.forEach(contact => {
                 const contactBadge = document.createElement("span");
                 contactBadge.classList.add("contact-badge");
-                contactBadge.innerHTML = contact + ' <button class="remove-btn">❎
-🗑️
-</button>';
+                contactBadge.innerHTML = contact + ' <button class="remove-btn">🗑️</button>';
                 contactBadge.style.cursor = "pointer";
                 contactBadge.onclick = function () {
                     removeContact(contact);
